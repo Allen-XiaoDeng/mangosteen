742f67d9222e89a177d3cff7d6e2f70bab3aa291
Allen <guolong613@LAPTOP-TJ3DRH1B.localdomain>
2022-08-20 22:10:28 +0800

    重新build

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/mangosteen/', //github 项目名称
+  base: '/mangosteen/dist/', //github 项目名称
   plugins: [vue()]
 })
