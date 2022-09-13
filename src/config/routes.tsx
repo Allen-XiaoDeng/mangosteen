a5b8ac00fe96261c9f8fbb34e3230ea60877ed15
guolong <guolong.deng@hand-china.com>
2022-09-13 13:34:33 +0800

    修改默认路由

--- src/config/routes.tsx
+++ src/config/routes.tsx
@@ -1,23 +1,21 @@
-import { Bar } from '../views/Bar'
-import { Foo } from '../views/Foo'
-import { RouteRecordRaw } from 'vue-router'
-import { First } from '../components/welcome/First'
-import { Second } from '../components/welcome/Second'
-import { Forth } from '../components/welcome/Forth'
-import { Third } from '../components/welcome/Third'
-import { Welcome } from '../views/Welcome'
+import { RouteRecordRaw } from 'vue-router';
+import { First } from '../components/welcome/First';
+import { Second } from '../components/welcome/Second';
+import { Forth } from '../components/welcome/Forth';
+import { Third } from '../components/welcome/Third';
+import { Welcome } from '../views/Welcome';
 
 export const routes: RouteRecordRaw[] = [
-  { path: '/', component: Foo },
-  { path: '/about', component: Bar },
-  {
-    path: '/welcome',
-    component: Welcome,
-    children: [
-      { path: '1', component: First, },
-      { path: '2', component: Second, },
-      { path: '3', component: Third, },
-      { path: '4', component: Forth, }
-    ]
-  }
-]
+	{ path: '/', redirect: '/welcome' },
+	{
+		path: '/welcome',
+		component: Welcome,
+		children: [
+			{ path: '', redirect: '/welcome/1' },
+			{ path: '1', component: First },
+			{ path: '2', component: Second },
+			{ path: '3', component: Third },
+			{ path: '4', component: Forth },
+		],
+	},
+];
