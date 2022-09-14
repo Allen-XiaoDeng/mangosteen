11356105cc37744a04145f8be953cc12eb4cab04
guolong <guolong.deng@hand-china.com>
2022-09-14 11:07:07 +0800

    封装useSwipe Hook

--- src/views/Welcome.tsx
+++ src/views/Welcome.tsx
@@ -1,32 +1,41 @@
-import { defineComponent, h, Transition, VNode } from 'vue';
+import { ref, watchEffect, defineComponent, h, Transition, VNode } from 'vue';
 import { RouterView, RouteLocationNormalizedLoaded } from 'vue-router';
 import s from './Welcome.module.scss';
-import logo from '../assets/icons/mangosteen.svg';
+import { useSwipe } from '../hooks/useSwipe';
 
 export const Welcome = defineComponent({
-  setup: (props, context) => {
-    return () => (
-      <div class={s.wrapper}>
-        <header>
-          <svg>
-            <use xlinkHref='#mangosteen'></use>
-          </svg>
-          <h1>山竹记账</h1>
-        </header>
-        <main class={s.main}>
-          <RouterView name="main">
-            {({ Component: X, route: R }: { Component: VNode, route: RouteLocationNormalizedLoaded }) =>
-              <Transition enterFromClass={s.slide_fade_enter_from} enterActiveClass={s.slide_fade_enter_active}
-                leaveToClass={s.slide_fade_leave_to} leaveActiveClass={s.slide_fade_leave_active}>
-                {X}
-              </Transition>
-            }
-          </RouterView>
-        </main>
-        <footer>
-          <RouterView name="footer" />
-        </footer>
-      </div>
-    )
-  }
-})
+	setup: (props, context) => {
+		const main = ref<HTMLElement | null>(null);
+		const { direction, swiping } = useSwipe(main);
+		watchEffect(() => {
+			console.log(swiping.value, direction.value);
+		});
+		return () => (
+			<div class={s.wrapper}>
+				<header>
+					<svg>
+						<use xlinkHref="#mangosteen"></use>
+					</svg>
+					<h1>山竹记账</h1>
+				</header>
+				<main class={s.main}>
+					<RouterView name="main">
+						{({ Component: X, route: R }: { Component: VNode; route: RouteLocationNormalizedLoaded }) => (
+							<Transition
+								enterFromClass={s.slide_fade_enter_from}
+								enterActiveClass={s.slide_fade_enter_active}
+								leaveToClass={s.slide_fade_leave_to}
+								leaveActiveClass={s.slide_fade_leave_active}
+							>
+								{X}
+							</Transition>
+						)}
+					</RouterView>
+				</main>
+				<footer>
+					<RouterView name="footer" />
+				</footer>
+			</div>
+		);
+	},
+});
