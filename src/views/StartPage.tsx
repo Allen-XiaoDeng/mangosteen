9c5c04b8b4bf4e5e3c752d7de4496619d833cbf0
guolong <guolong.deng@hand-china.com>
2022-09-15 13:52:20 +0800

    创建 Overlay 组件

--- src/views/StartPage.tsx
+++ src/views/StartPage.tsx
@@ -1,30 +1,37 @@
-import { defineComponent } from 'vue';
+import { defineComponent, ref } from 'vue';
 import { Button } from '../shared/Button';
 import { Center } from '../shared/Center';
 import { FloatButton } from '../shared/FloatButton';
 import { Icon } from '../shared/Icon';
 import { Navbar } from '../shared/Navbar';
+import { Overlay } from '../shared/Overlay';
 import s from './StartPage.module.scss';
 
 export const StartPage = defineComponent({
 	setup: (props, context) => {
-		const onClick = () => {
-			console.log('clicked');
+		const refOverlayVisible = ref(false);
+		const onClickMenu = () => {
+			refOverlayVisible.value = !refOverlayVisible.value;
+			console.log(refOverlayVisible.value);
 		};
 		return () => (
 			<div>
 				<nav>
-					<Navbar>{{ default: '山竹记账', icon: <Icon name="menu" class={s.navIcon} /> }}</Navbar>
+					<Navbar>
+						{{
+							default: () => '山竹记账',
+							icon: () => <Icon name="menu" class={s.navIcon} onClick={onClickMenu} />,
+						}}
+					</Navbar>
 				</nav>
 				<Center class={s.pig_wrapper}>
 					<Icon name="pig" class={s.pig} />
 				</Center>
 				<div class={s.button_wrapper}>
-					<Button class={s.button} onClick={onClick}>
-						开始记账
-					</Button>
-					<FloatButton iconName="add" />
+					<Button class={s.button}>开始记账</Button>
 				</div>
+				<FloatButton iconName="add" />
+				{refOverlayVisible.value && <Overlay onClose={() => (refOverlayVisible.value = false)} />}
 			</div>
 		);
 	},
