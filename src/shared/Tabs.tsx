2ecad5623568f03cbcd1174ecef81d86fe51c42a
Allen <guolong613@gmail.com>
2022-09-17 15:16:31 +0800

    扩展了 Tabs 组件，支持 classPrefix

--- src/shared/Tabs.tsx
+++ src/shared/Tabs.tsx
@@ -2,6 +2,9 @@ import { defineComponent, PropType } from 'vue';
 import s from './Tabs.module.scss';
 export const Tabs = defineComponent({
 	props: {
+		classPrefix: {
+			type: String
+		},
 		selected: {
 			type: String as PropType<string>,
 		},
@@ -12,26 +15,30 @@ export const Tabs = defineComponent({
 	},
 	setup: (props, context) => {
 		return () => {
-			const array = context.slots.default?.();
-			if (!array) return () => null;
-			for (let i = 0; i < array.length; i++) {
-				if (array[i].type !== Tab) {
+			const tabs = context.slots.default?.();
+			if (!tabs) return () => null;
+			for (let i = 0; i < tabs.length; i++) {
+				if (tabs[i].type !== Tab) {
 					throw new Error('Tabs的子组件必须是Tab');
 				}
 			}
+			const cp = props.classPrefix
+
 			return (
-				<div class={s.tabs}>
-					<ol class={s.tabs_nav}>
-						{array.map(item => (
+				<div class={[cp + '_tabs', s.tabs]}>
+					<ol class={[cp + '_tabs_nav', s.tabs_nav]}>
+						{tabs.map(item => (
 							<li
-								class={item.props?.name === props.selected ? s.selected : ''}
+								class={[
+									item.props?.name === props.selected ? [s.selected, cp + '_selected'] : '',
+									cp + '_tabs_nav_item']}
 								onClick={() => context.emit('update:selected', item.props?.name)}
 							>
 								{item.props?.name}
 							</li>
 						))}
 					</ol>
-					<div>{array.find(item => item.props?.name === props.selected)}</div>
+					<div>{tabs.find(item => item.props?.name === props.selected)}</div>
 				</div>
 			);
 		};
