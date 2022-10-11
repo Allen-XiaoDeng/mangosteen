fc6922a510bc586a5c1d22e548484083a6f7ffb7
guolong <guolong.deng@hand-china.com>
2022-10-11 14:07:36 +0800

    如果看过广告就不用看了

--- src/components/welcome/ForthActions.tsx
+++ src/components/welcome/ForthActions.tsx
@@ -1,11 +1,18 @@
 import s from './welcome.module.scss';
 import { RouterLink } from 'vue-router';
-export const ForthActions = () => (
-  <div class={s.actions}>
-    <RouterLink class={s.fake} to="/start" >跳过</RouterLink>
-    <RouterLink to="/start" >完成</RouterLink>
-    <RouterLink class={s.fake} to="/start" >跳过</RouterLink>
-  </div>
-)
+import { SkipFeatures } from '../../shared/SkipFeatures';
+const onClick = () => {
+	localStorage.setItem('skipFeatures', 'yes');
+};
 
-ForthActions.displayName = 'ForthActions'
+export const ForthActions = () => (
+	<div class={s.actions}>
+		<SkipFeatures class={s.fake} />
+		<span onClick={onClick}>
+			<RouterLink to="/start">完成</RouterLink>
+		</span>
+		<SkipFeatures class={s.fake} />
+	</div>
+);
+
+ForthActions.displayName = 'ForthActions';
