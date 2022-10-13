59fe77304790603719672e7ba6b1702c2ca0682f
guolong <guolong.deng@hand-china.com>
2022-10-13 15:48:14 +0800

    items 可以按照自定义时间查询

--- src/components/item/ItemSummary.tsx
+++ src/components/item/ItemSummary.tsx
@@ -1,4 +1,4 @@
-import { defineComponent, onMounted, PropType, ref, reactive } from 'vue';
+import { defineComponent, onMounted, PropType, ref, reactive, watch } from 'vue';
 import { FloatButton } from '../../shared/FloatButton';
 import { Button } from '../../shared/Button';
 import { http } from '../../shared/Http';
@@ -36,12 +36,23 @@ export const ItemSummary = defineComponent({
 			page.value += 1;
 		};
 		onMounted(fetchItems);
+		watch(
+			() => [props.startDate, props.endDate],
+			() => {
+				items.value = [];
+				hasMore.value = false;
+				page.value = 0;
+				fetchItems();
+			}
+		);
+
 		const itemsBalance = reactive({
 			expenses: 0,
 			income: 0,
 			balance: 0,
 		});
-		onMounted(async () => {
+
+		const fetchItemsBalance = async () => {
 			if (!props.startDate || !props.endDate) {
 				return;
 			}
@@ -52,7 +63,20 @@ export const ItemSummary = defineComponent({
 				_mock: 'itemIndexBalance',
 			});
 			Object.assign(itemsBalance, response.data);
-		});
+		};
+		onMounted(fetchItemsBalance);
+		watch(
+			() => [props.startDate, props.endDate],
+			() => {
+				Object.assign(itemsBalance, {
+					expenses: 0,
+					income: 0,
+					balance: 0,
+				});
+				fetchItemsBalance();
+			}
+		);
+
 		return () => (
 			<div class={s.wrapper}>
 				{items.value ? (
